8f1d1a7b92adbfc45679fe998505db3af4a51487
Hicham Bezzizi <hicham_bezzizi@hotmail.com>
2019-05-16 16:12:59 +0200

    added fetch to display api data

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+const options = {
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    headers: {'X-Auth-Token': '972c6683e59747d18bbbdf01008cacb2',},
+}
+
+const getTeam = () => {
+fetch('https://api.football-data.org/v2/teams/18', options)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    // console.log(JSON.stringify(myJson));
+    var data = JSON.stringify(myJson); 
+    document.querySelector("#footTeam").innerHTML = data //this displays it in HTML
+  });
+}
+getTeam(); //function call
+
+
