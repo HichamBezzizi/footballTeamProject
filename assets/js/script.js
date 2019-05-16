7b533e4f59fb82bc960700a32164df3798d280cb
Hicham Bezzizi <hicham_bezzizi@hotmail.com>
2019-05-16 16:42:58 +0200

    html and css work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
 const options = {
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    headers: {'X-Auth-Token': '972c6683e59747d18bbbdf01008cacb2',},
+    headers: {'X-Auth-Token': '972c6683e59747d18bbbdf01008cacb2'},
 }
 
 const getTeam = () => {
@@ -11,10 +11,14 @@ fetch('https://api.football-data.org/v2/teams/18', options)
   })
   .then(function(myJson) {
     // console.log(JSON.stringify(myJson));
-    var data = JSON.stringify(myJson); 
+    var data = JSON.stringify(myJson);
+    var playerName
+    var playerPos
+    var playerNat
+    var playerNumber
     document.querySelector("#footTeam").innerHTML = data //this displays it in HTML
   });
 }
-getTeam(); //function call
+// getTeam(); //function call
 
 
